a379f4e454627bf271bf93eeb13df31d1b39afb4
wealth iduwe <wealthiduwe@gmail.com>
2022-12-28 18:58:07 +0100

    Navigating Programmatically

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -5,12 +5,23 @@ import {useRouter} from "next/router";
 function Index(props:AppProps) {
 
 
-    const router= useRouter().query
+    const router= useRouter()
 
 
-    console.log(router)
+    console.log(router.query)
+
+
+    function loadClientProjects() {
+router.push('/clients/45/67')
+
+    }
     return (
-        <div>The Project of a given Client with id  {router.id}</div>
+        <div>
+
+         <h2>The Project of a given Client with id  {router.query.id}</h2>
+
+        <button onClick={loadClientProjects} >Load Projects</button>
+        </div>
     );
 }
 
