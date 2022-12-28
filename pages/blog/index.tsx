bef66afaf12a9774459987a34e3709631a40fb10
wealth iduwe <wealthiduwe@gmail.com>
2022-12-28 18:46:26 +0100

    Understanding the use cases of Link hrefs

---
+++ pages/blog/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {AppProps} from "next/app";
+
+function Index(props:AppProps) {
+    return (
+        <div>This is my blogs Page</div>
+    );
+}
+
+export default Index;
--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import {AppProps} from "next/app";
+import {useRouter} from "next/router";
+import Link from "next/link";
 
 function Index(props:AppProps) {
+
+const clients = [
+    {id:33, name:'Wealth'},
+    {id:44, name:'Chika'},
+
+]
     return (
-        <div>The Client page</div>
+        <div>
+
+        <h1> The Client page</h1>
+
+            {clients.map((item)=> (<li key={item.id}>
+
+                <Link href={{
+                    pathname: '/clients/[id]',
+                    query: {id: item.id}
+
+                }}>{item.name}</Link>
+                {/*<Link href={`/clients/${item.id}`}>{item.name}</Link>*/}
+            </li>))}
+        </div>
     );
 }
 
